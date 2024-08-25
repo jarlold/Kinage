4eb804b0590186e8dbcddf87a765e39e908e10b0
jarlold <jarlold@protonmail.com>
2024-08-24 21:14:12 -0400

    Added support for animations and the ability to set them

--- graphical_client.py
+++ graphical_client.py
@@ -3,6 +3,8 @@ import os
 import pyglet
 import simplified_client
 
+import magic
+
 import pyglet
 from pyglet.gl import *
 
@@ -12,23 +14,37 @@ keyboard = pyglet.window.key.KeyStateHandler()
 pyglet_resources = {}
 
 def load_all_resources():
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
     for i in os.listdir(simplified_client.resource_folder):
-        img = pyglet.image.load(simplified_client.resource_folder+i)
-        pyglet_resources[i] = img
+        # Build the path
+        pp = simplified_client.resource_folder+i
+
+        # If the file says its a gif, load it as an animation
+        if magic.from_file(pp)[:3] == 'GIF':
+            img = pyglet.image.load_animation(
+                pp,
+                decoder=pyglet.image.codecs.get_animation_decoders()[1]
+            )
+        # If he says he's a PNG load him as an image
+        elif magic.from_file(pp)[:3] == 'PNG': 
+            img = pyglet.image.load(simplified_client.resource_folder+i)
+        pyglet_resources[i] = pyglet.sprite.Sprite(x=0, y=0, img=img)
 
 @window.event
 def on_draw():
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
     window.clear()
-#   glPushMatrix()
-#   glTranslatef(window.width/2.0, window.height/2.0, 0)
-#   glTranslatef(-3*simplified_client.player_node.x, -3*simplified_client.player_node.y, 0)
-#   glScalef(3.0, 3.0, 1)
-    for nid in simplified_client.nodes:
-        node = simplified_client.nodes[nid]
-        pyglet_resources[node.texture_name].blit(node.x, node.y)
-#   glPopMatrix()
+    glPushMatrix()
+    glTranslatef(window.width/2.0, window.height/2.0, 0)
+    glTranslatef(-3*simplified_client.player_node.x, -3*simplified_client.player_node.y, 0)
+    glScalef(3.0, 3.0, 1)
+    nodes_to_draw = list(simplified_client.nodes.values())
+    nodes_to_draw.sort(key=lambda x: x.z)
+    for node in nodes_to_draw:
+        pyglet_resources[node.texture_name].x = node.x
+        pyglet_resources[node.texture_name].y = node.y
+        pyglet_resources[node.texture_name].draw()
+    glPopMatrix()
 
 def on_tick(dt):
     speed = dt * 40
